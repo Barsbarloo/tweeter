2b495b1534bd546cf6962436424b9721e4d379f9
Barsbarloo <lootanke@gmail.com>
2023-01-16 17:33:55 +0600

    updating route.rb
    set up root to Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/create'
-  get 'tweets/destroy'
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
 
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'tweets#index'
 end
