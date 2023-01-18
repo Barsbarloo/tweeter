060670b7aee3cd2b3ce7a8de08c6cf9b73211858
Barsbarloo <lootanke@gmail.com>
2023-01-18 16:15:42 +0600

    add user to controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
-# app/controllers/tweets_controller.rb
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc)
@@ -7,6 +7,8 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+
     respond_to do |format|
       if @tweet.save
         format.turbo_stream
@@ -14,21 +16,27 @@ class TweetsController < ApplicationController
         format.html do
           flash[:tweet_errors] = @tweet.errors.full_messages
           redirect_to root_path
+        end
       end
     end
   end
 
-
   def show
+ 
   end
 
   def destroy
+   
   end
+
+  def retweet
+    
+  end
+
 
   private
 
-  def tweet_params
-    params.require(:tweet).permit(:body)
-  end
-end
+    def tweet_params
+      params.require(:tweet).permit(:body)
+    end
 end
